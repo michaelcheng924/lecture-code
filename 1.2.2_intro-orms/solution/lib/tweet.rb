861616bfebbae56303856fbc88629944767822bf
michael.cheng <michael.cheng@NYC-US03879-MBP.local>
2018-07-04 15:56:46 -0400

    intro-orms solution

---
+++ 1.2.2_intro-orms/solution/lib/tweet.rb
@@ -0,0 +1,42 @@
+class Tweet
+  attr_accessor :message, :username
+  ALL = []
+
+  def self.all
+    sql = <<-SQL
+    SELECT * FROM tweets
+    SQL
+
+    DB[:conn].execute(sql).map do |tweet|
+      Tweet.new(tweet)
+    end
+  end
+
+  def initialize(props={})
+    @message = props['message']
+    @username = props['username']
+
+    @id = props['id']
+
+    ALL << self
+  end
+
+  def save
+    if(@id)
+      sql = <<-SQL
+      UPDATE tweets
+      SET message = ?
+      WHERE id = ?
+      SQL
+
+      DB[:conn].execute(sql, @message, @id)
+    else
+      sql = <<-SQL
+      INSERT INTO tweets (username, message)
+      VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, @username, @message)
+    end
+  end
+end
